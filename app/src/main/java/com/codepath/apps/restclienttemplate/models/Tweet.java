05cc5ca731ca557e5233b835a617f8fc9afcf69a
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-02-16 22:31:29 -0500

    Added the videos

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -2,15 +2,20 @@ package com.codepath.apps.restclienttemplate.models;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Tweet {
 
     public String body;
     public String createdAt;
     public User user;
+// Putting an empty constructor
+    public Tweet(){
+
+    }
 
     public static Tweet fromJson(JSONObject jsonObject) throws Exception{
         Tweet tweet = new Tweet();
